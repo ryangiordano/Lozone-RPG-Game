a2ca9515553b70f28e0199c1b93a47e6dc3312da
thedaruma <rgiorda1@gmail.com>
2019-04-17 17:10:12 -0400

    add api for attacking and defending on combatants

--- src/main/components/entities/Combatant.ts
+++ src/main/components/entities/Combatant.ts
@@ -5,6 +5,9 @@ enum Status {
   confused,
   fainted
 }
+enum ModifierStatType {
+  strength, stamina, speed, intellect, wisdom, dexterity, hp, mp
+}
 
 class Spell {
   public id: number;
@@ -12,29 +15,38 @@ class Spell {
 
   }
 }
-// For use with items and spells.
-class Effect {
-
-}
 class Modifier {
-  public id: number;
+  constructor(
+    public id: number,
+    public name: string,
+    public modifierStatType: ModifierStatType,
+    public modifierPotency: number) {
+
+  }
+}
+class Buff {
+  constructor(
+    public id: number,
+    public modifiers: Modifier[],
+    public name: string) {
+
+  }
 }
 class Behavior {
   public id: number;
 }
-export class CurrentStatus {
-  constructor(currentHP, currentMP, status, modifiers: Modifier[]) {
+// For use with items and spells.
+class Effect {
 
-  }
 }
 // export class Spell
 export class Combatant {
-  private modifiers: Modifier[];
+  private buffs: Map<number, Buff>;
   private behaviors: Map<number, Behavior>;
   private spells: Map<number, Spell>
   public currentHp: number;
   public currentMp: number;
-  public status: Map<number, Status>;
+  public status: Set<Status>;
   constructor(
     public id: number,
     public name: string,
@@ -42,14 +54,15 @@ export class Combatant {
     private hp: number,
     private mp: number,
     private level: number,
-    private attackPower: number,
-    private defensePower: number,
     private intellect: number,
     private dexterity: number,
-    private stength: number,
+    private strength: number,
     private wisdom: number,
     private stamina: number,
     spells?: Spell[]) {
+    this.status = new Set<Status>();
+    this.buffs = new Map<number, Buff>();
+
     this.spells = new Map<number, Spell>();
     if (spells) {
       spells.forEach(this.addSpell)
@@ -62,19 +75,44 @@ export class Combatant {
       }
     })
   }
-  initializeStatus({ currentHp, currentMp, statusArray, modifierArray }) {
+  initializeStatus(currentHp: number, currentMp: number, statusArray: Status[] = [], buffArray: Buff[] = []) {
     this.currentHp = currentHp || this.hp;
     this.currentMp = currentMp || this.mp;
-    this.status = statusArray || [];
-    this.modifiers = modifierArray || [];
+    statusArray.forEach(status => {
+      this.status.add(status);
+    });
+    buffArray.forEach(buff => {
+      if (!this.buffs.has(buff.id)) {
+        this.buffs.set(buff.id, buff);
+      }
+    });
   }
   addSpell(spell: Spell) {
-    if(!this.spells.has(spell.id)){
+    if (!this.spells.has(spell.id)) {
       this.spells.set(spell.id, spell);
     }
   }
   removeSpell(spellId) {
     this.spells.delete(spellId);
+  }
+  attackTarget(target: Combatant) {
+    const potency = this.getAttackPower();
+    target.receivePhysicalDamage(potency);
+
+  }
+  receivePhysicalDamage(potency:number){
+    const defensePotency = this.getDefensePower();
+    const actualDamage = Math.max(0, defensePotency - potency);
+  }
+  getAttackPower() {
+    //TODO: Factor in equipment as well
+    return this.strength * this.level;
+  }
+  getDefensePower(){
+    return this.stamina * this.level;
+  }
+  getModifierValue() {
+
   }
   private changeCurrent(property, value: number) {
     property = Math.min(property + value, property);
